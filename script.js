3136f4d25772902369718ba79d1fbb9813658bd1
anacardosolima17-creator <ana.cardoso.lima17@escola.pr.gov.br>
2026-08-31 12:38:44 -0300

    dfvdggs

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMontentLoaded",() => {
+    prepararReacoes();
+    prepararAnimacaoCard();
+    criarBotaoTopo();
+})
+
+
+function prepararReacoes() {
+    const artigos = document.querySelectorAll("article");
+
+    artigos.forEach((artigo, indice) => {
+
+        const botoes = artigo.querySelectorAll("button");
+
+        if (botoes.lenght < 2){
+            return;
+        }
+
+        const botaoUM = botoes[0];
+        const botaoDOIS = botoes[1];
+
+        const contadorBotaoUM = botaoUM.querySelector("span");
+        const contadorBotaoDOIS = botao.botaoDOIS.querySelector("span");
+        const idCard = `card-${indice + 1}`;
+
+        const chaveBotaoUM  = `${idCard}-botaoUM`;
+        const chaveBotaoDOIS  = `${idCard}-botaoDOIS`;
+
+        let UM = Number(localStorage.getItem(chaveBotaoUM)) || 0;
+        let DOIS = Number(localStorage.getItem(chaveBotaoDOIS)) || 0;
+
+        contadorBotaoUM.textContent = UM;
+        contadorBotaoDOIS.textContent = DOIS;
+    })
+
+
+}
